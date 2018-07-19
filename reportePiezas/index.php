db6270c8564dc03ba701f27611b2837eac7485c4
USERPC <USERPC@Lenovo-PC>
2018-07-18 22:27:11 -0500

    se subio el cascaron del proyecto favor de verificar el codigo

--- reportePiezas/index.php
+++ reportePiezas/index.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-           echo 'Hola mundo';
-        ?>
-    </body>
-</html>
+<?php
+	require 'app/controller/mvc.controller.php'; 
+	$mvc = new mvc_controller();	
+		$mvc->principal();
+?>
